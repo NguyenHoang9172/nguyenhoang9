6d0ade8d960d3d0736e579ee5b60405c1cbb025a
NguyenHoang9172 <hanhnguyenhoang270199@gmail.com>
2025-09-25 21:23:39 +0700

    more files to add

---
+++ if_elif_else.py
@@ -0,0 +1,13 @@
+diem_tb = 7.5
+if diem_tb >= 8 :
+    print("Hoc luc gioi")
+elif diem_tb >=6.5 :
+    print("Hoc luc kha")
+elif diem_tb >=5 :
+    print('Hoc luc trung binh')
+else:
+    print('Hoc lai')
+
+
+
+tesstttttttttt
